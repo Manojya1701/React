98ca370e661638e2f4e24e6289eb59832fb5305a
Manojya1701 <manojya.guruju@gmail.com>
2026-01-04 11:53:06 +0530

    impo
    rt React from "react";dd package.json and package-lock.json
    
    import { createRoot } from "react-dom/client";
    
    const heading = React.createElement(
      "h1",
      { id: "heading" },
      "HI from react 19"
    );
    
    const root = createRoot(document.getElementById("root"));
    root.render(heading);

---
+++ README.md
@@ -0,0 +1,8 @@
+#parcel
+bundler
+compresser
+hmr hot module replacemnt
+useus file watching algorithm written in c++
+image optimization
+parcel-cache
+minification of ifles
--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const heading = React.createElement("h1",
-    {id:"heading"},
-    "HI from react"
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "HI from react 19"
 );
-const root=ReactDOM.createRoot(document.getElementById("root"));
+
+const root = createRoot(document.getElementById("root"));
 root.render(heading);
