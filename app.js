c11480bd9f39fd8e938ddd3704c54fc82b1d5603
Manojya1701 <manojya.guruju@gmail.com>
2026-02-17 22:40:29 +0530

    Leraned react element and react functional components

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
- //Recat Crete elemnt
-const heading=React.createElement(
-  "h1",
-  {id:"heading"},
-  "HI from REact element create"
-);
-//JSX
-const jsxheading=<h1>Namastey react from jSX</h1>;
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+import ReactDOM from "react-dom/client";
+ //React Element
+ const heading = (
+    <h1 className="head" tabIndex="5">
+    Namsate
+    </h1>
+ );
+ const Title = () => (
+   <h1>Hello</h1>
+
+ );
+ //React functional Component
+ //componentcomposition
+ const HeadingComponent = () => (
+  <div id="container">
+    
+    <Title/>
+    <h1 className="heading">Namastey</h1>
+  </div>
+ );
+ const root=ReactDOM.createRoot(document.getElementById("root"));
+ root.render(heading);// rendering a react element
+ root.render(<HeadingComponent/>); // rendering a react functional component
